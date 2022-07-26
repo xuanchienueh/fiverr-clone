f91bdaef0856a3ee8b6a40f8a6677412aef4458c
xuanchienueh <xuanchien.ueh@gmail.com>
2022-07-27 04:25:30 +0700

    done page login and page register

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,15 @@
-import { logoSvg } from "images/imageSvg";
 import React, { useEffect, useState } from "react";
-import { Button, Nav, Navbar } from "react-bootstrap";
+import { Button, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
 import { LinkContainer } from "react-router-bootstrap";
-import { Link } from "react-router-dom";
 import styles from "./header.module.scss";
 import SearchHeader from "./searchHeader/SearchHeader";
+import { logoSvg } from "images/imageSvg";
 import TypeJobs from "./typeJobs/TypeJobs";
 
 function Header() {
+  const { infoUserLogin } = useSelector((state) => state.manageUserReducer);
   return (
     <div className={` ${styles.header}`}>
       <Navbar bg="light" expand="xl" variant="light">
@@ -26,10 +28,36 @@ function Header() {
             <Nav.Link href="#">English</Nav.Link>
             <Nav.Link href="#">$ USD</Nav.Link>
             <Nav.Link href="#">Become a Seller</Nav.Link>
-            <Nav.Link href="#">Sign in</Nav.Link>
-            <Nav.Link href="#">
-              <Button variant="outline-success">John</Button>
-            </Nav.Link>
+
+            {infoUserLogin.token ? (
+              <NavDropdown title={`Welcome ${infoUserLogin?.user?.name}`} id="dropdown-account">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item
+                  onClick={() => {
+                    localStorage.clear();
+                    window.location.reload();
+                  }}
+                >
+                  Sign out
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : null}
+
+            {!infoUserLogin.token ? (
+              <>
+                <LinkContainer to="/login">
+                  <Nav.Link>Sign in</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/register">
+                  <Nav.Link>
+                    <Button variant="outline-success">John</Button>
+                  </Nav.Link>
+                </LinkContainer>
+              </>
+            ) : null}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
---
+++ src/pages/login/Login.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, useNavigate, Link } from "react-router-dom";
+
+import styles from "./login.module.scss";
+import { memberLoginAction } from "redux/manageUser/actionCallApi";
+
+const Schema = Yup.object().shape({
+  email: Yup.string().email("Email is invalid!").required("Email is required!"),
+  password: Yup.string().required("Password is required!"),
+});
+
+export default function Login() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { infoUserLogin } = useSelector((state) => state.manageUserReducer);
+
+  console.log(infoUserLogin);
+  const formik = useFormik({
+    initialValues: {
+      password: "",
+      email: "",
+    },
+    validationSchema: Schema,
+    onSubmit: (values) => {
+      dispatch(memberLoginAction(values, navigate));
+    },
+  });
+  if (Object.entries(infoUserLogin).length > 0) {
+    return <Navigate to="/" replace={true} />;
+  }
+  return (
+    <div className={styles.login}>
+      <div className="form-login">
+        <h1 className="text-center mb-3">Member Login</h1>
+        <form onSubmit={formik.handleSubmit} className="">
+          <div className="form-group">
+            <label htmlFor="email">Email address:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+            {formik.errors.email && formik.touched.email && (
+              <div className="text-danger">** {formik.errors.email}</div>
+            )}
+          </div>
+          <div className="form-group">
+            <label htmlFor="pwd">Password:</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            />
+          </div>
+
+          <div className="text-center">
+            <button type="submit" className="btn px-5 btn-outline-success">
+              Login
+            </button>
+          </div>
+          <div className="d-flex mt-3 justify-content-center">
+            <h5 className="mr-2">Don't have an account? </h5>
+            <Link className="h5" to="/register">
+              Register!
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
