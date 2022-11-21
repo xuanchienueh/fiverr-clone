2fb02960f09c742b3e4ec8181798fc6a3456e866
Nguyễn Xuân Chiến <97378366+xuanchienueh@users.noreply.github.com>
2022-11-21 15:08:41 +0700

    Update config.js
    thay token

--- src/utils/setting/config.js
+++ src/utils/setting/config.js
@@ -1,13 +1,16 @@
 const DOMAIN_API = "https://fiverr.cybersoft.edu.vn/api";
 const TOKEN = "accessToken";
 const USER_LOGIN = "USER_LOGIN";
-const tokenByClass =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyNSIsIkhldEhhblN0cmluZyI6IjE2LzEyLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY3MTE0ODgwMDAwMCIsIm5iZiI6MTY0MTU3NDgwMCwiZXhwIjoxNjcxMjk2NDAwfQ.cB7cdIfS0TKI1Yx_WRS-tEOt5K5yf3QJCot63SYEOHo";
+// const tokenByClass =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyNSIsIkhldEhhblN0cmluZyI6IjE2LzEyLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY3MTE0ODgwMDAwMCIsIm5iZiI6MTY0MTU3NDgwMCwiZXhwIjoxNjcxMjk2NDAwfQ.cB7cdIfS0TKI1Yx_WRS-tEOt5K5yf3QJCot63SYEOHo";
 
+const tokenByClass =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udGVuZCA3MyIsIkhldEhhblN0cmluZyI6IjIzLzA1LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY4NDgwMDAwMDAwMCIsIm5iZiI6MTY1OTg5MTYwMCwiZXhwIjoxNjg0OTQ3NjAwfQ.u471oZWr9EMgIb7oeyuaxfi8spgAgUuTkUHYSS9pBWg";
 function imgNotFound(e) {
   return (
     (e.target.onerror = null),
-    (e.target.src = "https://www.rabrotech.com/upload/default/image-not-found.png")
+    (e.target.src =
+      "https://www.rabrotech.com/upload/default/image-not-found.png")
   );
 }
 
